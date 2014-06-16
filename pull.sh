cebe43f31c26b750c814296ff3e9492b8a4d6fdf
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-06-16 22:07:50 +0200

    No need to track linaro anymore
    Change-Id: I5d03e09f88a8944e5a02a14f0e36647a25c2ee78

--- pull.sh
+++ pull.sh
@@ -4,14 +4,14 @@
 
 croot
 cd abi/cpp
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd art
 git pull upstream kk4.4
 croot
 cd bionic
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd bootable/recovery
 git pull upstream kk4.4
@@ -21,7 +21,7 @@ git pull upstream kk4.4
 croot
 cd dalvik
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 
 # Device
 croot
@@ -38,27 +38,27 @@ git pull upstream kk4.4
 croot
 cd external/bluetooth/bluedroid
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/chromium
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/clang
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/dnsmasq
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/e2fsprogs
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/elfutils
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/embunit
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/exfat
 git pull upstream kk4.4
@@ -67,108 +67,108 @@ cd external/fuse
 git pull upstream kk4.4
 croot
 cd external/freetype
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/gtest
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/iproute2
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/iputils
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/linux-tools-perf
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/llvm
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/openssh
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/openssl
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/oprofile
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/skia
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/sonivox
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/srec
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/stlport
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/tcpdump
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/v8
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/valgrind
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/webrtc
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd external/wpa_supplicant_8
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 
 # Frameworks
 croot
 cd frameworks/av
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd frameworks/base
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd frameworks/compile/slang
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd frameworks/ex
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd frameworks/native
 git pull upstream kk4.4
 croot
 cd frameworks/opt/net/voip
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd frameworks/opt/telephony
 git pull upstream kk4.4
 croot
 cd frameworks/rs
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd frameworks/wilhelm
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 
 # Hardware
 croot
 cd hardware/libhardware
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd hardware/libhardware_legacy
 git pull upstream kk4.4
 croot
 cd hardware/ril
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 
 # Kernel
 #croot
@@ -179,13 +179,13 @@ git pull linaro linaro_android_4.4.2
 croot
 cd libcore
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 
 # Apps
 croot
 cd packages/apps/Camera
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd packages/apps/Dialer
 git pull upstream kk4.4
@@ -197,17 +197,17 @@ cd packages/apps/InCallUI
 git pull upstream kk4.4
 croot
 cd packages/apps/LegacyCamera
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd packages/inputmethods/LatinIME
 git pull upstream kk4.4
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd packages/apps/Launcher3
 git pull upstream android-4.4
 croot
 cd packages/inputmethods/OpenWnn
-git pull linaro linaro_android_4.4.2
+#git pull linaro linaro_android_4.4.2
 croot
 cd packages/apps/PerformanceControl
 git pull upstream android-4.4
