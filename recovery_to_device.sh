ceaad8c14a2654fa2e0fc222c53c6b2b3a065dd3
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-07-17 22:53:22 +0200

    Update scripts

--- recovery_to_device.sh
+++ recovery_to_device.sh
@@ -8,11 +8,10 @@ DESTPATH="$1"
 
 # Get Package Name
 DEVICE=`echo $OUT | grep -Eo '[^/]+/?$' | cut -d / -f1`
-PLATFORM_VERSION=`sed -n -e'/ro.build.version.release/s/^.*=//p' $OUT/system/build.prop`
-VERSION=SlimKernel-$DEVICE-$PLATFORM_VERSION-$(date +"%Y%m%d")
+VERSION=SlimRecovery-$DEVICE-$(date +"%Y%m%d")
 PACKAGE=$OUT/$VERSION.zip
 if [ -z "$DESTPATH" ]; then
-    DESTPATH=/sdcard/SlimKat
+    DESTPATH=/sdcard/SlimBean
 fi
 
 echo -e "${cya}Waiting for device... ${txtrst}"
