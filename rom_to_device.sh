8684a8697a622618e0e0349df04a5d19a4907f7e
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-12-16 22:35:40 +0100

    adb: overcome insufficient permissions

--- rom_to_device.sh
+++ rom_to_device.sh
@@ -10,14 +10,17 @@ DESTPATH="$1"
 VERSION=`sed -n -e'/ro.modversion/s/^.*=//p' $OUT/system/build.prop`
 PACKAGE=$OUT/$VERSION.zip
 if [ -z "$DESTPATH" ]; then
-    DESTPATH=/sdcard/SlimKat
+    DESTPATH=/sdcard/SlimPop/
 fi
 
+adb=$(which adb)
+$adb kill-server
+sudo $adb start-server
 echo -e "${cya}Waiting for device... ${txtrst}"
-adb wait-for-device
+$adb wait-for-device
 
 echo -e "${cya}Sending $VERSION.zip to ${DESTPATH}... ${txtrst}"
-adb push -p ${PACKAGE} ${DESTPATH}
-adb push ${PACKAGE}.md5 ${DESTPATH}
+$adb push -p ${PACKAGE} ${DESTPATH}
+$adb push ${PACKAGE}.md5sum ${DESTPATH}
 
 croot
